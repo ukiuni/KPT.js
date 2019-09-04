5a714e58f528c7f9b306d137ffb37cce439582ba
ukiuni <ukiuni@gmail.com>
2019-09-04 22:49:32 +0900

    bugfix: sortalbe broken

--- routes/items.js
+++ routes/items.js
@@ -4,72 +4,72 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + '/../config/config.json')[env];
 var express = require('express');
 var router = express.Router();
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
 	Project.findOne({
-		where : {'key': req.body.projectKey}
-	}).then(function(project) {
+		where: { 'key': req.body.projectKey }
+	}).then(function (project) {
 		if (!project) {
 			res.status(404).send();
 			return;
 		}
 		project.increment({
-			itemIncrements : 1
-		}).then(function(project) {
+			itemIncrements: 1
+		}).then(function (project) {
 			Item.create({
-				projectKey : project.key,
-				key : global.UUID.create(),
-				status : req.body.status,
-				index : parseInt(req.body.index),
-				number : project.itemIncrements
-			}).then(function(item) {
+				projectKey: project.key,
+				key: global.UUID.create(),
+				status: req.body.status,
+				index: parseInt(req.body.index),
+				number: project.itemIncrements
+			}).then(function (item) {
 				res.json(item);
-			}).catch(function(error) {
+			}).catch(function (error) {
 				console.log("error = " + error);
 				res.status(500).json(error);
 			});
-		}).catch(function(error) {
+		}).catch(function (error) {
 			console.log("error = " + error);
 			res.status(500).json(error);
 		});
-	}).catch(function(error) {
+	}).catch(function (error) {
 		console.log("error = " + error);
 		res.status(500).json(error);
 	});
 });
-router.put('/', function(req, res) {
+router.put('/', function (req, res) {
 	var requestItem = req.body;
 	Item.findOne({
-		where : {
-			key : requestItem.key
+		where: {
+			key: requestItem.key
 		}
-	}).then(function(item) {
+	}).then(function (item) {
 		if (null == item) {
-			res.json({message:"item is null"});
+			res.json({ message: "item is null" });
 			return;
 		}
 		item.title = requestItem.title;
 		item.description = requestItem.description;
 		item.status = parseInt(requestItem.status);
 		item.index = parseInt(requestItem.index);
-		item.save().then(function(item) {
+		item.save().then(function (item) {
 			res.json(item);
 			sendEvent(item.projectKey, "update", item);
-		}).catch(function(error) {
+		}).catch(function (error) {
 			res.json(error);
 		});
 	})
 });
-router.put('/move', function(req, res) {
+router.put('/move', function (req, res) {
 	var movedItem = req.body.item;
 	Item.findAll({
-		where : {
-			projectKey : movedItem.projectKey,
-			status : movedItem.status
+		where: {
+			projectKey: movedItem.projectKey,
+			status: movedItem.status
 		},
-		order : [ 'index' ]
-	}).then(function(items) {
+		order: ['index']
+	}).then(function (items) {
 		var index = 0;
-		for ( var i in items) {
+		for (var i in items) {
 			var sortItem = items[i];
 			if (sortItem.key == movedItem.key) {
 				continue;
@@ -79,42 +79,44 @@ router.put('/move', function(req, res) {
 			} else {
 			}
 			sortItem.index = index++;
-			sortItem.save().then(function() {
-			}).catch(function(err) {
+			sortItem.save().then(function () {
+			}).catch(function (err) {
 				console.log("err = " + err)
 			});
 		}
 		Item.update(movedItem, {
-			key : movedItem.key
-		}).then(function() {
-		}).catch(function(err) {
+			where: {
+				key: movedItem.key
+			}
+		}).then(function () {
+		}).catch(function (err) {
 			console.log("err = " + err);
 		});
 	});
 	sendEvent(req.body.projectKey, "move", movedItem);
 	res.json({
-		message : "accepted"
+		message: "accepted"
 	});
 });
-router['delete']('/', function(req, res) {
+router['delete']('/', function (req, res) {
 	if (Array.isArray(req.body)) {
 		var items = req.body
 	} else {
-		var items = [ req.body ]
+		var items = [req.body]
 	}
-	items.forEach(function(item) {
+	items.forEach(function (item) {
 		Item.findOne({
-			where : {
-				key : item.key
+			where: {
+				key: item.key
 			}
-		}).then(function(findedItem) {
+		}).then(function (findedItem) {
 			if (null == findedItem) {
 				return;
 			}
-			findedItem.destroy().then(function() {
+			findedItem.destroy().then(function () {
 				if (1 == items.length) {
 					res.json({
-						message : "accepted"
+						message: "accepted"
 					});
 				}
 				sendEvent(item.projectKey, "delete", item);
@@ -126,13 +128,13 @@ if (config.redis) {
 	var redis = require('redis');
 	var publisher = redis.createClient(config.redis.port, config.redis.host);
 }
-var sendEvent = function(projectKey, event, item) {
+var sendEvent = function (projectKey, event, item) {
 	if (!config.redis) {
 		global.io.sockets["in"](projectKey).emit(event, item);
 	} else {
 		publisher.publish(projectKey, JSON.stringify({
-			event : event,
-			item : item
+			event: event,
+			item: item
 		}));
 	}
 }
